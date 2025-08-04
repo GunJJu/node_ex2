571dc822f25944a2525d7ed28df6ba1d6e0a55f4
GunJJu <smkim0721@gmail.com>
2025-08-04 12:09:10 +0900

    init

---
+++ index.js
@@ -0,0 +1,105 @@
+const express = require("express")
+const app = express()
+const PORT = 3000
+
+app.use(express.json())
+
+// 가상의 사용자 배열
+let users = [
+    { id: 1, name: "홍길동" },
+    { id: 2, name: "김철수" },
+    { id: 3, name: "홍경복" }
+];
+
+app.post("/users", (req, res) => {
+    try {
+        const newUser = req.body
+
+        users.push({
+            id: Date.now(),
+            ...newUser
+        })
+        res.status(201).json({ message: "사용자 추가 완료", users })
+
+    } catch (error) {
+        console.log("사용자 추가 중 오류", error)
+        res.status(500).json({ message: "서버 오류 발생" })
+    }
+})
+
+app.get("/users", (req, res) => {
+    try {
+        res.json(users)
+        res.status(200).json({ message: "가져오기 성공!" })
+    } catch (error) {
+        console.log("사용자 조회 중 오류..!", error)
+        res.status(500).json({ message: "서버 내부 오류 발생..!" })
+    }
+})
+
+app.get("/users/:id", (req, res) => {
+    try {
+        const userId = Number(req.params.id)
+        const index = users.findIndex(u => u.id === userId)
+
+        if (index === -1) {
+            return res.status(404).json({ message: "조회할 사용자가 없습니다." })
+        }
+
+        res.status(200).json({ message: "1명 데이터 조회 완료", user: users[index] })
+
+    } catch (error) {
+        console.log("사용자 조회 중 오류..!", error)
+        res.status(500).json({ message: "서버 내부 오류 발생..!" })
+    }
+})
+
+app.put("/users/:id", (req, res) => {
+    try {
+        const userId = Number(req.params.id)
+        const index = users.findIndex(u => u.id === userId)
+
+        if (index === -1) {
+            return res.status(404).json({ message: "조회할 사용자가 없습니다." })
+        }
+
+        const updateData = req.body
+        users[index] = {
+            ...users[index],
+            ...updateData
+        }
+
+        res.status(200).json({ message: "1명 데이터 조회 완료", user: users[index] })
+
+    } catch (error) {
+        console.log("사용자 조회 중 오류..!", error)
+        res.status(500).json({ message: "서버 내부 오류 발생..!" })
+    }
+})
+
+app.delete("/users/:id", (req, res) => {
+    try {
+        const userId = Number(req.params.id)
+
+        const index = users.findIndex(u => u.id == userId)
+
+        if (index === -1) {
+            return res.status(404).json({ message: "삭제할 사용자가 없습니다." })
+        }
+
+        users.splice(index, 1)
+        res.status(201).json({ message: "사용자 1명 삭제 완료", users })
+    } catch (error) {
+        console.error("사용자 삭제 중 오류", error)
+        res.status(500).json({ message: "서버 내부 오류 발생" })
+    }
+})
+
+
+app.get("/", (req, res) => {
+    res.send("Hello world!")
+})
+
+app.listen(PORT, () => {
+    console.log("Server is running...")
+})
